23578862eb11cf84d20bd6cf115c21b0128ffc7a
caicojesus <cjsboletos@gmail.com>
2025-12-05 23:17:19 -0300

    Site sobre marmita
    criação do site sobre marmitas

--- JS/botaohamburger.js
+++ JS/botaohamburger.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 1. Espera o HTML carregar completamente
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -10,19 +9,5 @@ document.addEventListener('DOMContentLoaded', () => {
     menuHamburger.addEventListener('click', () => {
         // 4. Quando clicado, ele adiciona ou remove a classe 'menu-visivel'
         navMenu.classList.toggle('menu-visivel');
+        });
     });
-=======
-// 1. Espera o HTML carregar completamente
-document.addEventListener('DOMContentLoaded', () => {
-
-    // 2. Encontra o botão hambúrguer e o menu
-    const menuHamburger = document.getElementById('menu-hamburger');
-    const navMenu = document.querySelector('.holographic-container');
-
-    // 3. Adiciona um "ouvinte de clique" no botão
-    menuHamburger.addEventListener('click', () => {
-        // 4. Quando clicado, ele adiciona ou remove a classe 'menu-visivel'
-        navMenu.classList.toggle('menu-visivel');
-    });
->>>>>>> 7c2dfabfffd86ee7725cb72427e017ac25ec42d7
-});
